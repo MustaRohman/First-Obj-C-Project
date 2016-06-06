ffccfd9eb433c32ba60ea77c262676ff78e6dbb5
Musta Rohman <mustarohman@gmail.com>
2016-06-06 15:53:07 +0100

    Initial Commit

---
+++ FirstProject/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FirstProject
+//
+//  Created by Musta Rohman on 06/06/2016.
+//  Copyright © 2016 Musta Rohman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
